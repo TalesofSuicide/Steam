16c667335d4f6bdceee1f69aa76a64ad7d56a07d
TalesofSuicide <105525987+TalesofSuicide@users.noreply.github.com>
2024-06-21 23:54:41 -0700

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [1245620];  // Enter here AppIDs of the needed games
+var games = [1245620, 230410, 553850, 381210];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
