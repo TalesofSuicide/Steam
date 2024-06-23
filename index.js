d69787ac5298cbc839709b1f4b0ed32f202cd94f
TalesofSuicide <105525987+TalesofSuicide@users.noreply.github.com>
2024-06-22 20:59:30 -0700

    index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [1245620, 230410, 553850, 381210];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
