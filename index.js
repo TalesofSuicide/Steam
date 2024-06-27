51737edc20fc0b5601cbae533659d97f23d28d38
TalesofSuicide <105525987+TalesofSuicide@users.noreply.github.com>
2024-06-27 03:08:38 -0700

    index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [814380, 1245620, 230410, 553850, 381210];  // AppIDs of the games you want to play
+var games = [814380, 1245620, 230410, 553850, 381210];  // Enter here AppIDs of the needed games
+var nonSteamGames = ["Playing with Sui the Raccoon"];  // Custom non-Steam game name
 var status = 1;  // 1 - online, 7 - invisible
-var customStatus = "Playing with Sui the Raccoon";  // Custom status message
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
     if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-    user.setPersona(status, customStatus);  // Set persona with custom status
-    user.gamesPlayed(games);  // Set games being played
+    user.setPersona(status); 
+    user.gamesPlayed([...games, ...nonSteamGames]); // Combine Steam games and non-Steam games
 });
