a702aae4b55f9004bf396ba322eb9dd71a489e2c
TalesofSuicide <105525987+TalesofSuicide@users.noreply.github.com>
2024-06-27 02:39:02 -0700

    index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
-const { exec } = require('child_process');
 const keep_alive = require('./keep_alive.js');
 
 var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [814380, 1245620, 230410, 553850, 381210];  // Enter the AppIDs of the needed games
-var nonSteamGame = "Playing with Sui the raccoon";  // Non-Steam game name
+var games = [814380, 1245620, 230410, 553850, 381210];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
+var nonSteamGame = { game_id: '0', game_extra_info: 'Playing with Sui the raccoon' };  // Non-Steam game
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
     if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-    user.setPersona(status);               
-    user.gamesPlayed([{ game_id: '0', game_extra_info: nonSteamGame }, ...games.map(game_id => ({ game_id }))]);  // Include the non-Steam game without the prefix
-
-    // Optionally launch non-Steam game
-    exec('path_to_your_non_steam_game_executable', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error launching non-Steam game: ${error}`);
-            return;
-        }
-        console.log(`Non-Steam game output: ${stdout}`);
-    });
+    user.setPersona(status);
+    user.gamesPlayed([nonSteamGame, ...games.map(game_id => ({ game_id }))]);
 });
