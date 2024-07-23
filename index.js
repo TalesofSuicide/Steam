0d37fd611d01df1b3be89abc11f5eadc2cd68516
TalesofSuicide <105525987+TalesofSuicide@users.noreply.github.com>
2024-07-23 01:14:37 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,23 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [814380, 1245620, 230410, 553850, 381210, 72850];  // Enter the AppIDs of the needed games
-var nonSteamGames = ["Mr.Krabs Plays", "A Small Violin"];  // List of non-Steam game names
+var games = [814380, 1245620, 230410, 553850, 381210,72850];  // Enter the AppIDs of the needed games
+var nonSteamGame = "Touching Nick";  // Non-Steam game name
 var status = 1;  // 1 - online, 7 - invisible
 
 user = new steamUser();
 user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
     if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-    user.setPersona(status);
-    
-    let currentGameIndex = 0;
-    user.gamesPlayed([nonSteamGames[currentGameIndex], ...games]);  // Show the first non-Steam game initially
-
-    // Function to change the non-Steam game name every 5 seconds
-    setInterval(() => {
-        currentGameIndex = (currentGameIndex + 1) % nonSteamGames.length;
-        user.gamesPlayed([nonSteamGames[currentGameIndex], ...games]);
-    }, 5000);
+    user.setPersona(status);               
+    user.gamesPlayed([nonSteamGame, ...games]);  // Show the non-Steam game first, then the other games
 
     // Launch non-Steam game (if needed)
     exec('path_to_your_non_steam_game_executable', (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error launching non-Steam game: ${error}`);
+            console.error(Error launching non-Steam game: ${error});
             return;
         }
-        console.log(`Non-Steam game output: ${stdout}`);
+        console.log(Non-Steam game output: ${stdout});
     });
 });
